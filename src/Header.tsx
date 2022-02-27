adba751cc723461b38b70e825d88cca2c59e82e0
Uge Hidalgo <ugehidalgo@gmail.com>
2022-02-27 19:27:37 +0100

    Styles for some components

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,8 @@
-import styles from './Header.module.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import React from 'react';
 import { UserIcon } from './Icons';
+import { fontFamily, fontSize, gray1, gray2, gray5 } from './Styles';
 
 //Function-based component with implicit return
 export const Header = () => {
@@ -8,16 +10,75 @@ export const Header = () => {
     console.log(e.currentTarget.value);
   };
   return (
-    <div className={styles.container}>
-      <a href="./">Q & A </a>
+    <div
+      css={css`
+        position: fixed;
+        box-sizing: border-box;
+        top: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px 20px;
+        background-color: #fff;
+        border-bottom: 1px solid ${gray5};
+        box-shadow: 0 3px 7px 0 rgba(110, 112, 114, 0.21);
+      `}
+    >
+      <a
+        href="./"
+        css={css`
+          font-size: 24px;
+          font-weight: bold;
+          color: ${gray1};
+          text-decoration: none;
+        `}
+      >
+        Q & A{' '}
+      </a>
       <input
         type="text"
         placeholder="Search..."
         onChange={handleSearchInputChange}
+        css={css`
+          box-sizing: border-box;
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          padding: 8px 10px;
+          border: 1px solid ${gray5};
+          border-radius: 3px;
+          color: ${gray2};
+          background-color: white;
+          width: 200px;
+          height: 30px;
+          :focus {
+            outline-color: ${gray5};
+          }
+        `}
       />
-      <a href="./signin">
+      <a
+        href="./signin"
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          padding: 5px 10px;
+          background-color: transparent;
+          color: ${gray2};
+          text-decoration: none;
+          cursor: pointer;
+          :focus {
+            outline-color: ${gray5};
+          }
+        `}
+      >
         <UserIcon />
-        <span>Sign In</span>
+        <span
+          css={css`
+            margin-left: 7px;
+          `}
+        >
+          Sign In
+        </span>
       </a>
     </div>
   );
