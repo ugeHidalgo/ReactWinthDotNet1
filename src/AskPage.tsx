dd3be4c699d423c7bdee17598be22baa6fdb75fd
Uge Hidalgo <ugehidalgo@gmail.com>
2022-03-26 18:10:33 +0100

    Added validations for ask page.

--- src/AskPage.tsx
+++ src/AskPage.tsx
@@ -1,7 +1,8 @@
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Page } from './Page';
 import {
   FieldContainer,
+  FieldError,
   FieldInput,
   Fieldlabel,
   FieldSet,
@@ -17,18 +18,48 @@ interface IFormData {
 }
 
 export const AskPage = () => {
-  const { register } = useForm<IFormData>();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<IFormData>({
+    mode: 'onBlur',
+  });
+
+  const onSubmit: SubmitHandler<IFormData> = (data) => {
+    console.log(data);
+    console.log(errors);
+  };
+
   return (
     <Page title="Ask a question">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FieldSet>
           <FieldContainer>
             <Fieldlabel htmlFor="title">Title</Fieldlabel>
-            <FieldInput {...register('title')} id="title" type="text" />
+            <FieldInput
+              {...register('title', { required: true, minLength: 10 })}
+              id="title"
+            />
+            {errors.title?.type === 'required' && (
+              <FieldError>El título es necesario</FieldError>
+            )}
+            {errors.title?.type === 'minLength' && (
+              <FieldError>
+                La longitud mínima del título debe ser 10 caracteres.
+              </FieldError>
+            )}
           </FieldContainer>
+
           <FieldContainer>
             <Fieldlabel htmlFor="content">Content</Fieldlabel>
-            <FieldTextArea {...register('content')} id="content" />
+            <FieldTextArea
+              {...register('content', { required: true })}
+              id="content"
+            />
+            {errors.content?.type === 'required' && (
+              <FieldError>A content is required</FieldError>
+            )}
           </FieldContainer>
           <FormButtonContainer>
             <PrimaryButton type="submit">Submit your question</PrimaryButton>
