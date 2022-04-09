adf9859d1bfb5201dc7c863103a4f6df94812616
Uge Hidalgo <ugehidalgo@gmail.com>
2022-04-09 17:17:48 +0200

    Connected search page to redux store to handle searchings state

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -5,14 +5,24 @@ import { QuestionList } from './QuestionList';
 import { searchQuestions, QuestionData } from './QuestionData';
 import { Page } from './Page';
 import React from 'react';
-import { fontSize } from './Styles';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  AppState,
+  searchingQuestionsAction,
+  searchedQuestionsAction,
+  gettingQuestionAction,
+} from './Store';
 
 export const SearchPage = () => {
   //Destructure search params
   const [searchParams] = useSearchParams();
 
   //State to hold matched questions
-  const [questions, setQuestions] = React.useState<QuestionData[]>([]);
+  //Without Redux
+  //const [questions, setQuestions] = React.useState<QuestionData[]>([]);
+  //With Redux
+  const dispatch = useDispatch();
+  const questions = useSelector((state: AppState) => state.questions.searched);
 
   //Getting the query parameter 'criteria'
   const search = searchParams.get('criteria') || '';
@@ -20,11 +30,17 @@ export const SearchPage = () => {
   //Invoke the search when the component is rendered the first time and every change o the search variable
   React.useEffect(() => {
     const doSearch = async (criteria: string) => {
+      //With redux
+      dispatch(gettingQuestionAction());
       const foundResults = await searchQuestions(criteria);
-      setQuestions(foundResults);
+      //Without redux
+      //setQuestions(foundResults);
+      //With redux
+      dispatch(searchedQuestionsAction(foundResults));
       console.log(`Found ${foundResults.length} coincidences.`);
     };
     doSearch(search);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
   return (
     <Page title="Search results">
